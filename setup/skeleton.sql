596641d214ca96098af4c9a314dff060510d4a0c
Thomas Epperson <tepperson@briskheat.com>
2017-07-06 12:23:24 -0400

    Update the sql skeleton file with changes to the mysql tables.

--- setup/skeleton.sql
+++ setup/skeleton.sql
@@ -1,46 +1,73 @@
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `contacts` (
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.2
+-- http://www.phpmyadmin.net
+--
+-- Host: sql312.rf.gd
+-- Generation Time: Jul 06, 2017 at 12:23 PM
+-- Server version: 5.6.35-81.0
+-- PHP Version: 5.3.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `rfgd_20333149_webtest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE IF NOT EXISTS `contacts` (
   `emp_id` int(11) NOT NULL AUTO_INCREMENT,
-  `last_name` varchar(255) NOT NULL,
-  `middle_name` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
   `username` varchar(32) NOT NULL,
   `password` char(64) DEFAULT NULL,
-  `salt` char(64) NOT NULL,
-  `stretching` int(11) NOT NULL,
+  `salt` char(64) DEFAULT NULL,
+  `stretching` int(11) DEFAULT NULL,
   `fail_logins` int(11) NOT NULL DEFAULT '0',
   `fail_pass_change` int(11) NOT NULL DEFAULT '0',
   `classification` varchar(25) NOT NULL DEFAULT 'Employee',
   `payment_eligible` tinyint(1) NOT NULL DEFAULT '0',
-  `ssn` varchar(11) NOT NULL,
-  `phone_mobile` varchar(25) NOT NULL,
-  `phone_home` varchar(25) NOT NULL,
-  `phone_other` varchar(25) NOT NULL,
-  `website` varchar(320) NOT NULL,
+  `phone_mobile` varchar(25) DEFAULT NULL,
+  `phone_home` varchar(25) DEFAULT NULL,
+  `phone_other` varchar(25) DEFAULT NULL,
+  `website` varchar(320) DEFAULT NULL,
   `email` varchar(320) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `state` varchar(5) NOT NULL,
-  `zipcode` varchar(5) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(5) DEFAULT NULL,
+  `zipcode` varchar(5) DEFAULT NULL,
   UNIQUE KEY `emp_id` (`emp_id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `contact_permission` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_permission`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_permission` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id1` int(11) DEFAULT NULL,
-  `id2` int(11) NOT NULL,
+  `id2` int(11) DEFAULT NULL,
   `permission` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id1` (`id1`,`id2`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cost_estimations` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cost_estimations`
+--
+
+CREATE TABLE IF NOT EXISTS `cost_estimations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `cost_type` varchar(25) NOT NULL,
@@ -55,11 +82,15 @@ CREATE TABLE `cost_estimations` (
   `employees` int(11) NOT NULL,
   `notes` varchar(255) NOT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `equipment` (
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+
+CREATE TABLE IF NOT EXISTS `equipment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner` int(11) NOT NULL,
   `location` int(11) NOT NULL,
@@ -70,38 +101,30 @@ CREATE TABLE `equipment` (
   `last_known` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `img_id` int(11) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=579 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `images` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=580 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uploader` int(11) NOT NULL,
   `size` int(11) DEFAULT NULL,
-  `file_vga` varchar(255) NOT NULL,
-  `file_thumb` varchar(255) NOT NULL,
+  `file_vga` varchar(255) DEFAULT NULL,
+  `file_thumb` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `inspections` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `prop_id` int(11) NOT NULL,
-  `type` varchar(25) NOT NULL,
-  `inspector` int(11) NOT NULL,
-  `referrer` int(11) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `paid_by` int(11) NOT NULL,
-  `comments` varchar(255) NOT NULL,
-  `report` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `jobs` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `jobs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cust_billing` int(11) NOT NULL,
   `cust_shipping` int(11) NOT NULL,
@@ -112,32 +135,56 @@ CREATE TABLE `jobs` (
   `comments` text,
   `invoice` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `job_status` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_expenses`
+--
+
+CREATE TABLE IF NOT EXISTS `job_expenses` (
+  `job_id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL,
+  `expense` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_status`
+--
+
+CREATE TABLE IF NOT EXISTS `job_status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `jobid` int(11) NOT NULL,
   `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `what_happened` varchar(255) DEFAULT NULL,
   `new_status` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `job_tasks` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `job_tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `datetime` datetime NOT NULL,
   `job` int(11) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `locations` (
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE IF NOT EXISTS `locations` (
   `id` int(32) NOT NULL AUTO_INCREMENT,
   `owner` int(11) NOT NULL,
   `position` int(32) NOT NULL DEFAULT '0',
@@ -145,11 +192,15 @@ CREATE TABLE `locations` (
   `location` varchar(255) NOT NULL,
   `img_id` int(11) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=111 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `payments` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=114 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE IF NOT EXISTS `payments` (
   `payment_id` int(11) NOT NULL AUTO_INCREMENT,
   `pay_to` int(11) NOT NULL,
   `paid_by` int(11) NOT NULL,
@@ -161,43 +212,53 @@ CREATE TABLE `payments` (
   `inspection` int(11) DEFAULT NULL,
   `invoice` varchar(255) DEFAULT NULL,
   UNIQUE KEY `payment_id` (`payment_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `properties` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(255) NOT NULL,
-  `state` varchar(2) NOT NULL,
-  `zip` varchar(10) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `status` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE IF NOT EXISTS `status` (
   `id` int(32) NOT NULL,
   `item` int(11) NOT NULL,
   `status` varchar(16) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `status_codes` (
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_codes`
+--
+
+CREATE TABLE IF NOT EXISTS `status_codes` (
   `code` int(11) NOT NULL AUTO_INCREMENT,
   `Description` varchar(255) NOT NULL,
   UNIQUE KEY `code` (`code`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `version` (
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction_categories`
+--
+
+CREATE TABLE IF NOT EXISTS `transaction_categories` (
+  `code` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `version`
+--
+
+CREATE TABLE IF NOT EXISTS `version` (
   `id` varchar(255) NOT NULL,
   `num` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-INSERT INTO `version` VALUES ('this',1),('contacts',1),('contact_permission',1),('cost_estimations',1),('equipment',1),('images',1),('inspections',1),('jobs',1),('job_status',1),('job_tasks',1),('locations',1),('payments',1),('properties',1),('status',1),('status_codes',1);
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
