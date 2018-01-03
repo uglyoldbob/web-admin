3e456b26e4e67826c1a89d7d59a0b1a8119fa271
Thomas Epperson <tepperson@briskheat.com>
2018-01-03 15:32:19 -0500

    Try to introduce a unit test.

---
+++ unit_tests/OneTest.php
@@ -0,0 +1,13 @@
+ <?php
+namespace UnitTestFiles\Test;
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testTrueCheck()
+	{
+		$condition = true;
+		$this->assertTrue($condition);
+	}
+}
+?>
