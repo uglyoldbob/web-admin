aa700a6010975ded1eebbcebe0db7846c4b32170
Thomas Epperson <tepperson@briskheat.com>
2018-01-16 12:07:09 -0500

    Remove currently unused php-fpm and apache configuration. Add a test to check loading site configuration.

---
+++ unit_tests/indexTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace UnitTestFiles\Test;
+use PHPUnit\Framework\TestCase;
+
+require_once("web/global.php");
+
+class indexTest extends TestCase
+{
+
+    public function testLoadConfig()
+	{
+		$config = parse_ini_file("config.ini");
+		$this->assertNotEquals($config, FALSE);
+	}
+
+	/**
+	 * @depends testLoadConfig
+	 */
+	public function testDbConnect()
+	{
+		$this->assertTrue(true);
+	}
+}
+?>
