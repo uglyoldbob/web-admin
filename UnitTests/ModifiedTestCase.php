a954c941257deae60684163601d5d897b94e72d0
Thomas Epperson <thomas.epperson@gmail.com>
2018-02-01 13:04:36 -0500

    Add a customization of TestCase and test it with contactsTest.php

---
+++ UnitTests/ModifiedTestCase.php
@@ -0,0 +1,87 @@
+<?php
+namespace UnitTestFiles\Test;
+use PHPUnit\Framework\TestCase;
+
+class ModifiedTestCase extends TestCase
+{
+	protected $config_name = "config.ini";
+	protected $session_file = "./session_id.txt";
+	
+	protected $session_id;
+	protected $errors;
+	
+	protected function setUp()
+	{
+		chdir("./web");
+		$this->clearErrors();
+		set_error_handler(array($this, "errorHandler"));
+		if ($this->session_id = @file_get_contents($this->session_file))
+		{
+			session_id($this->session_id);
+		}
+		else
+		{
+			file_put_contents($this->session_file, session_id());
+		}
+	}
+	
+	protected function tearDown()
+	{
+		//handle cases where session_id is regenerated
+		file_put_contents($this->session_file, session_id());
+		session_write_close();
+		session_unset();
+	}
+	
+	public function errorHandler($errno, $errstr, $errfile, $errline, $errcontext)
+	{
+		$this->errors[] = compact("errno", "errstr", "errfile", "errline");
+	}
+	
+	public function clearErrors()
+	{
+		$this->errors = array();
+	}
+	
+	public function assertError($errstr, $errno)
+	{
+		$state = "ERRORS:\n";
+		foreach ($this->errors as $error)
+		{
+			$state .= $err['errno'] . ", " . $err['errstr'] . ", " . $err['errfile'] . ", " . $err['errline'] . "\n";
+			if ($error["errstr"] === $errstr && $error["errno"] === $errno)
+			{
+				$this->assertTrue(true);
+				return;
+			}
+		}
+		$this->fail("Error with level " . $errno . " and message '" . $errstr . "' not found in:\n" . $state);
+	}
+	
+	public function assertNoErrors()
+	{
+		if (count($this->errors) > 0)
+		{
+			$state = "ERRORS:\n";
+			foreach ($this->errors as $err)
+			{
+				$state .= $err['errno'] . ", " . $err['errstr'] . ", " . $err['errfile'] . ", " . $err['errline'] . "\n";
+			}
+
+			$this->fail("Errors were found where none were expected:\n" . $state);
+		}
+		$this->assertTrue(true);
+	}
+
+	public function testAssertErrors1()
+	{
+		trigger_error("Triggered error");
+		$this->assertError("Triggered error", E_USER_NOTICE);
+	}
+	
+	public function testAssertErrors3()
+	{
+		$this->assertNoErrors();
+	}
+}
+?>
---
+++ UnitTests/contactsTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace UnitTestFiles\Test;
+use PHPUnit\Framework\TestCase;
+
+class paymentsTest extends ModifiedTestCase
+{	
+	private $test_user = "testuser";
+	private static $test_pw = "";
+	private $test_email = "test@testing.com";
+
+	public static function setUpBeforeClass()
+	{
+		//generate a password for testing
+		//not actually cryptographically secure or sufficiently random
+		paymentsTest::$test_pw = substr(md5(rand()), 0, 16);
+	}
+
+	public function testPage1()
+	{
+		require_once("contacts.php");
+		$this->assertNoErrors();
+	}
+}
+?>
